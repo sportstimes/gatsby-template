b94375f04d0e5a71caa8fa3860ad60ce698a9278
Si Jobling <simon.jobling@gmail.com>
2019-08-05 19:54:21 +0100

    Update page headings

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
+    <h1>Hi from the second page <span role="img" aria-label="Wave">👋</span></h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
