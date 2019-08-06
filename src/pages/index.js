8b6ff29eef28d93f3cf1be15922e2b9a93d34585
Si Jobling <simon.jobling@gmail.com>
2019-08-06 08:16:08 +0100

    Set order of posts to ascending date

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,7 @@ const IndexPage = ({
 export default IndexPage
 export const pageQuery = graphql`
   query {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+    allMarkdownRemark(sort: { order: ASC, fields: [frontmatter___date] }) {
       edges {
         node {
           id
