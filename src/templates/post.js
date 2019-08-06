9a6328309a1a6192e6342805f53b150aa1a51e8d
Si Jobling <simon.jobling@gmail.com>
2019-08-06 08:16:22 +0100

    Apply layout and SEO to post template

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,18 +1,24 @@
 import React from "react"
 import { graphql } from "gatsby"
 
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
 export default function Template({ data }) {
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
   return (
-    <div className="blog-post">
-      <h1>{frontmatter.title}</h1>
-      <h2>{frontmatter.date}</h2>
-      <div
-        className="blog-post-content"
-        dangerouslySetInnerHTML={{ __html: html }}
-      />
-    </div>
+    <Layout>
+      <SEO title="Home" />
+      <div className="blog-post">
+        <h1>{frontmatter.title}</h1>
+        <h2>{frontmatter.date}</h2>
+        <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </div>
+    </Layout>
   )
 }
 
