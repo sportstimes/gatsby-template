3358ce262a5cb6990b22c8c5eddd3f47d8fd392f
Si Jobling <simon.jobling@gmail.com>
2019-08-07 18:14:02 +0100

    Set site title to competition start times

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Kick Off Times Starter`,
+    title: `Competition Start Times`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@si`,
   },
