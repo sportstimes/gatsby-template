76ea42d5d6324ab5732a9c976b57d06d95073bb9
Si Jobling <simon.jobling@gmail.com>
2019-08-08 08:50:32 +0100

    Install and config JSON output

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,54 @@ module.exports = {
         path: `${__dirname}/src/content`,
       },
     },
+    {
+      resolve: `gatsby-plugin-json-output`,
+      options: {
+        siteUrl: `https://example.com`,
+        graphQLQuery: `
+          {
+            allMarkdownRemark(limit: 1000) {
+              edges {
+                node {
+                  excerpt
+                  html
+                  fields { path }
+                  frontmatter {
+                    title
+                    created
+                    updated
+                  }
+                }
+              }
+            }
+          }
+        `,
+        serialize: results => results.data.allMarkdownRemark.edges.map(({ node }) => ({
+          path: node.fields.path, // MUST contain a path
+          title: node.frontmatter.title,
+          created: node.frontmatter.created,
+          updated: node.frontmatter.updated,
+          html: node.html,
+        })),
+        // feedMeta: {
+        //   author: {
+        //     name: author,
+        //   },
+        //   description: siteDescription,
+        //   favicon: `${siteUrl}/icons/icon-48x48.png`,
+        //   title: siteTitle,
+        // },
+        serializeFeed: results => results.data.allMarkdownRemark.edges.map(({ node }) => ({
+          id: nodes.field.path,
+          page_url: siteUrl + node.fields.path,
+          title: node.frontmatter.title,
+          date_published: new Date(node.frontmatter.created).toISOString(),
+          date_modified: new Date(node.frontmatter.updated).toISOString(),
+          excerpt: node.excerpt,
+        })),
+        nodesPerFeedFile: 100,
+      }
+    }    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
