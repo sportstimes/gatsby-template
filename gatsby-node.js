f8b72786e4c016e15849d49f2664c570b15d23f2
Si Jobling <simon.jobling@gmail.com>
2019-08-09 17:29:03 +0100

    Node script to generate RSS

--- gatsby-node.js
+++ gatsby-node.js
@@ -40,3 +40,18 @@ exports.createPages = async ({ actions, graphql }) => {
     })
   })
 }
+
+// RSS Feed generation
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
