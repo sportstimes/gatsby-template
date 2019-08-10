bbd2bad3c30c8ab8d493d9f9081c5eb6e915bfd6
Si Jobling <simon.jobling@gmail.com>
2019-08-10 20:11:04 +0100

    Import link for RSS

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { Link } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
