518ffa8747ba3c0e9c5b8cc8af52b3b4fd5b2ff0
Si Jobling <simon.jobling@gmail.com>
2019-08-20 08:06:55 +0100

    Output ICS as event.ics

--- gen-cal.js
+++ gen-cal.js
@@ -1,3 +1,4 @@
+const { writeFileSync } = require('fs')
 const ics = require('ics')
  
 const event = {
@@ -24,28 +25,6 @@ ics.createEvent(event, (error, value) => {
   }
  
   console.log(value)  
-  // BEGIN:VCALENDAR
-  // VERSION:2.0
-  // CALSCALE:GREGORIAN
-  // PRODID:adamgibbons/ics
-  // METHOD:PUBLISH
-  // X-PUBLISHED-TTL:PT1H
-  // BEGIN:VEVENT
-  // UID:d9e5e080-d25e-11e8-806a-e73a41d3e47b
-  // SUMMARY:Bolder Boulder
-  // DTSTAMP:20181017T204900Z
-  // DTSTART:20180530T043000Z
-  // DESCRIPTION:Annual 10-kilometer run in Boulder\, Colorado
-  // URL:http://www.bolderboulder.com/
-  // GEO:40.0095;105.2669
-  // LOCATION:Folsom Field, University of Colorado (finish line)
-  // STATUS:CONFIRMED
-  // CATEGORIES:10k races,Memorial Day Weekend,Boulder CO
-  // ORGANIZER;CN=Admin:mailto:Race@BolderBOULDER.com
-  // ATTENDEE;RSVP=TRUE;ROLE=REQ-PARTICIPANT;PARTSTAT=ACCEPTED;CN=Adam Gibbons:mailto:adam@example.com
-  // ATTENDEE;RSVP=FALSE;ROLE=OPT-PARTICIPANT;DIR=https://linkedin.com/in/brittanyseaton;CN=Brittany
-  //   Seaton:mailto:brittany@example2.org
-  // DURATION:PT6H30M
-  // END:VEVENT
-  // END:VCALENDAR
+  writeFileSync(`${__dirname}/event.ics`, value)
+
 })
