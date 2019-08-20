ac0ce80eadd07ef78c2af6e5eb448fc23eca56ce
Si Jobling <simon.jobling@gmail.com>
2019-08-20 18:45:45 +0100

    Add ICS generation to Gatsby Node

--- gatsby-node.js
+++ gatsby-node.js
@@ -55,3 +55,45 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
+
+// ICS generation
+console.log('Generating ICS…')
+const ics = require(`ics`)
+const { writeFileSync } = require(`fs`)
+let moment = require(`moment`)
+
+let events = [
+  {
+    start: moment('2019-07-10T18:00Z').format('YYYY-M-D-H-m').split("-"),
+    duration: { hours: 0, minutes: 30 },
+    title: 'First ICS event',
+    description: 'Getting ICS node package integrated with this repo',
+    location: 'Virgin Train, West Coast Mainline',
+    url: 'https://kickofftimestemplate.netlify.com/',
+    categories: ['Code', 'Side Projects', 'Kick Off'],
+    status: 'CONFIRMED',
+    organizer: { name: 'Si Jobling', email: 'simon.jobling@gmail.com' },
+  },
+  {
+    start: [2019, 8, 20, 8, 20],
+    duration: { hours: 0, minutes: 30 },
+    title: 'Second ICS event',
+    description: 'Creating a second event',
+    location: 'London Euston Train Station',
+    url: 'https://kickofftimestemplate.netlify.com/',
+    categories: ['Code', 'Side Projects', 'Kick Off'],
+    status: 'CONFIRMED',
+    organizer: { name: 'Si Jobling', email: 'simon.jobling@gmail.com' },
+  },
+]
+ 
+ics.createEvents(events, (error, value) => {
+  if (error) {
+    console.log(error)
+    return
+  }
+ 
+  console.log(value)  
+  writeFileSync(`${__dirname}/event.ics`, value)
+
+})
