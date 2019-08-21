22bbee99e652500aa711c9f1f5a6525d78fb5bee
Si Jobling <simon.jobling@gmail.com>
2019-08-21 18:28:33 +0100

    Try `<time>` element around event list date

--- src/components/event-row.js
+++ src/components/event-row.js
@@ -7,9 +7,9 @@ const EventRow = ({ post }) => (
       <td className="summary">
         <Link className="url" to={post.frontmatter.path}>{post.frontmatter.title}</Link>
       </td>
-      <td className="dtstart" title={moment(post.frontmatter.date).format("YYYY-MM-DDTHH:mm:ssZ")}>
-        🗓 {moment(post.frontmatter.date).format("DD MMM 'YY HH:mm")}
-      </td>
+      <td><time className="dtstart" datetime={moment(post.frontmatter.date).format("YYYY-MM-DDTHH:mm:ssZ")}>
+        {moment(post.frontmatter.date).format("DD MMM 'YY HH:mm")}
+      </time></td>
       <td className="location">
         {post.frontmatter.locationName}
       </td>
