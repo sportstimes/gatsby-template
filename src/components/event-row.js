c646987a5cb3af176af1ea34c805b89c106fc18b
Si Jobling <simon.jobling@gmail.com>
2019-08-21 18:35:15 +0100

    Try hiding timestamp for event list µF dtstart

--- src/components/event-row.js
+++ src/components/event-row.js
@@ -2,14 +2,17 @@ import React from "react"
 import { Link } from "gatsby"
 import moment from "moment"
 
+import "./event-row.css"
+
 const EventRow = ({ post }) => (
   <tr className="vevent" id={post.id}>
       <td className="summary">
         <Link className="url" to={post.frontmatter.path}>{post.frontmatter.title}</Link>
       </td>
-      <td><time className="dtstart" datetime={moment(post.frontmatter.date).format("YYYY-MM-DDTHH:mm:ssZ")}>
+      <td>
         {moment(post.frontmatter.date).format("DD MMM 'YY HH:mm")}
-      </time></td>
+        <time className="dtstart">{moment(post.frontmatter.date).format("YYYY-MM-DDTHH:mm:ssZ")}</time>
+      </td>
       <td className="location">
         {post.frontmatter.locationName}
       </td>
