b76c45dacc2fd4674d7eabe163079bebdcc0a605
Si Jobling <simon.jobling@gmail.com>
2019-08-22 08:16:12 +0100

    Microsoft update & clone link on ABOUT

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,14 +45,14 @@ const IndexPage = ({
         <p>The Sports Times is a boilerplate template for providing sports event calendars in open formats for easier consumption. It publishes dates, times &amp; locations using standards that most platforms understand.</p>
         <dl>
           <dt><span role="img" aria-label="Calendar">📆️</span> ICS</dt>
-          <dd>Calendar feeds directly into macOS, iOS, Google Android and Microsoft Outlook</dd>
+          <dd>Calendar feeds directly into macOS, iOS, Google Android, Microsoft Windows &amp; Office</dd>
           <dt><span role="img" aria-label="Satellite">📡</span> RSS</dt>
           <dd>Really Simple Syndication for content aggregators, online and native apps</dd>
           <dt><span role="img" aria-label="Document">📄</span> CSV</dt>
           <dd>Comma-separated content for personal use in the likes of Excel</dd>
         </dl>
         <h3>How do you use it?</h3>
-        <p>If you want to publish your own calendar of events for others to reference with ease, clone the repo, customise to your hearts content and host on your own website or domain.</p>
+        <p>If you want to publish your own calendar of events for others to reference with ease, <Link to="https://github.com/kickofftimes/gatsby-template">clone the repo</Link>, customise to your hearts content and host on your own website or domain.</p>
         <p>It's an <Link to="https://github.com/kickofftimes/gatsby-template">open-source project on Github</Link> for others to use and improve. Fork it, use it, raise a pull-request with your suggestions or simply <Link to="https://github.com/kickofftimes/gatsby-template/issues">raise an issue</Link> with ideas or problems.</p>
       </div>
      </Layout>
