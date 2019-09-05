87891406a357849b12ce2149c4a3a29cbca00246
Si Jobling <simon.jobling@gmail.com>
2019-09-05 07:39:17 +0100

    Set header background to green

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#003300`,
       marginBottom: `1.45rem`,
     }}
   >
