19ace9624b7df4f3b41ee537149cff9aecdb8906
Si Jobling <simon.jobling@gmail.com>
2019-09-11 07:54:24 +0100

    Configure Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -157,6 +157,26 @@ module.exports = {
         ],
       },
     },
+
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-91583-61",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "rugbyworldcuptimes.com",
+      },
+    },
+
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
