cec18d1ca006e602db46eb8d6813743442fc7aa1
Si Jobling <simon.jobling@gmail.com>
2019-09-25 18:09:40 +0100

    Create list template

---
+++ src/templates/list.js
@@ -0,0 +1,96 @@
+import React from "react"
+import { graphql } from "gatsby"
+import EventRow from "../components/event-row"
+import LocalTimezone from "../components/local-timezone"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+  const Events = edges
+    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map(edge => <EventRow key={edge.node.id} post={edge.node} />)
+
+  return (
+    <Layout>
+      <SEO title="All the games from Japan" />
+      <h1>All the games from Japan</h1>
+
+      <p>
+        <span role="img" aria-label="Spiral calendar">🗓</span> 
+        <a href="webcal://rugbyworldcuptimes.com/events.ics" className="ics">Subscribe to all match times in your iOS, MacOS and Office calendar</a> 
+        <small
+          style={{
+            display: `block`,
+          }}
+        >
+          or 
+          {` `}
+          <a href="https://support.google.com/calendar/answer/37100?hl=en"
+            style={{
+              textDecoration: `underline`,
+              background: `none`,
+              color: `rgba(255,255,255,0.8)`
+            }}>Google Calendar</a>
+        </small>
+      </p>
+
+      {LocalTimezone}
+
+      <table>
+        <thead>
+          <tr>
+            <th>Summary</th>
+            <th>When</th>
+            <th class="location">Where</th>
+            <th class="description">What</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Events}
+        </tbody>
+      </table>
+      <p>
+        <span role="img" aria-label="Download">⬇️</span>
+        Export as 
+        {` `}
+        <a href="/events.ics" className="ics" download="download">ICS</a>,
+        {` `}
+        <a href="/feed.xml" className="rss">RSS feed</a>
+        {` and `}
+        <a href="/events.csv" className="csv" download="download">CSV file</a>
+      </p>
+      <div id="about">
+      </div>
+     </Layout>
+  )
+}
+
+export default IndexPage
+export const pageQuery = graphql`
+  query($tag: String) {
+    allMarkdownRemark(
+      sort: { order: ASC, fields: [frontmatter___date] }
+      filter: { frontmatter: { tags: { in: [$tag] } } }
+    ) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date
+            path
+            title
+            locationName
+            matchNumber
+            group
+          }
+        }
+      }
+    }
+  }
+` 
