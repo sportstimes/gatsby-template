ae40b444b4c27db2e593c93d458ad4ebea154ace
Si Jobling <simon.jobling@gmail.com>
2019-09-26 18:00:49 +0100

    Simplify empty tags logic

--- src/templates/post.js
+++ src/templates/post.js
@@ -12,7 +12,8 @@ import kebabCase from "lodash/kebabCase"
 export default function Template({ data }) {
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
-  const tags = frontmatter.tags || {}
+  const tags = frontmatter.tags 
+
   return (
     <Layout>
       <SEO title={frontmatter.title} />
@@ -35,12 +36,13 @@ export default function Template({ data }) {
         />
         <h2>Want more?</h2>
         <ul className="inline">
-        {
+        { 
+          tags ? 
           tags.map(tag => (
           <li key={tag.fieldValue}>
             <Link to={`/${kebabCase(tag)}/`}>{tag} matches</Link>
           </li>
-          ))
+          )) : ''
         }
         <li><Link to="/">All matches</Link></li>
         </ul>
