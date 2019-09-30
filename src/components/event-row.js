c0a267792e13a25ae59cd33b9f6a6d19fca5d79e
Si Jobling <simon.jobling@gmail.com>
2019-09-30 12:07:24 +0100

    Fix matchNumber on event row ID

--- src/components/event-row.js
+++ src/components/event-row.js
@@ -5,7 +5,7 @@ import moment from "moment"
 import "./event-row.css"
 
 const EventRow = ({ post }) => (
-  <tr id={'Match' + post.matchNumber} className={ "vevent" + (moment(post.frontmatter.date).isBefore() ? " past" : "") }>
+  <tr id={'Match' + post.frontmatter.matchNumber} className={ "vevent" + (moment(post.frontmatter.date).isBefore() ? " past" : "") }>
       <td className="summary">
         <Link className="url" to={post.frontmatter.path}>{post.frontmatter.title}</Link>
       </td>
