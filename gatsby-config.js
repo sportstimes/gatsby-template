5af17b909debc1be7de210f6c1ce93405abed211
Si Jobling <si@jobling.com>
2024-01-31 21:58:53 +0000

    Change Google Fonts to Michroma

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
-          'Kelly Slab',
+          'Michroma',
           //`source sans pro\:300,400,400i,700` // you can also specify font weights and styles
         ],
         display: 'swap'
