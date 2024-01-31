406d7ecf74ebaae150695562b1e73f14a902f38b
Si Jobling <si@jobling.com>
2024-01-31 21:40:30 +0000

    Remove GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -158,25 +158,6 @@ module.exports = {
       },
     },
 
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "G-CMDQEKRZLE",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "6nationscalendar.com",
-      },
-    },
-
-
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
